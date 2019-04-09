9eae7388bde20111b41cd03b133d1b557401a5f0
Bruno Berisso <bruno.berisso@experoinc.com>
2019-04-09 16:48:08 +0100

    Add JSON parsing

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,14 +1,24 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DeriveGeneric #-}
+
 module Lib (getMessages) where
 
-import qualified Data.ByteString as BS
-import Data.Aeson (Value)
+import GHC.Generics
+import Data.ByteString.Char8 (pack)
+import Data.Aeson (FromJSON, Value, Object, (.:))
+import Data.Aeson.Types
 import Network.HTTP.Simple
 import Network.HTTP.Client
 import Network.HTTP.Client.TLS
 
-type ChannelId = BS.ByteString
-data Message = Message String String
+type ChannelId = String
+
+data Message = Message {
+    user :: String,
+    text :: String
+} deriving (Generic, Show)
+
+instance FromJSON Message
 
 setupRequestManager :: IO ()
 setupRequestManager = do
@@ -18,17 +28,25 @@ setupRequestManager = do
 -- "xoxp-198580037300-198456467459-200151962098-c14a436eefc7d812886e53b6786a18eb"
 -- "C5UJEF537"
 
-getMessages :: ChannelId -> IO ()
+slackAccountToken = "xoxp-198580037300-198456467459-200151962098-c14a436eefc7d812886e53b6786a18eb"
+
+getMessages :: ChannelId -> IO ([Message])
 getMessages channelId = do
-    let request = getRequest channelId "xoxp-198580037300-198456467459-200151962098-c14a436eefc7d812886e53b6786a18eb"
+    let request = getRequest (pack channelId) slackAccountToken
     response <- httpJSON request
-    putStrLn $ show (getResponseBody response :: Value)
+    messages <- case parseResponseBody response of
+        Right m  -> return m
+        Left e -> fail e
+    putStrLn $ show messages
+    return messages
     where
         getRequest channel token = setRequestHost "slack.com"
                                     $ setRequestPath "/api/channels.history"
                                     $ setRequestMethod "GET"
                                     $ setRequestSecure True
                                     $ setRequestPort 443
-                                    $ setRequestQueryString [("token", Just token), ("channel", Just channel)]
+                                    $ setRequestQueryString [("token", Just token), ("channel", Just channel), ("count", Just "5")]
                                     $ defaultRequest
+        parseResponseBody response = let body = getResponseBody response
+                                    in parseEither (.: "messages") body 
 
